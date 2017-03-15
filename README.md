29c9d827bf9f7ac2136e7b43d416c32be8421460
smarcu <smarcu@gmail.com>
2017-03-15 09:34:43 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-reactor-demo2
 Spring Reactor Demo 2
+
+example based on https://spring.io/blog/2016/07/20/notes-on-reactive-programming-part-iii-a-simple-http-server-application
+
+
+## Git tags for presentation:
---
+++ src/main/java/com/example/SpringReactorDemo2Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class SpringReactorDemo2Application {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SpringReactorDemo2Application.class);
+
+	public static void main(String[] args) throws Exception {
+
+
+	}
+}
